216ad647e58463eec9b3b7ed00f58b5d65a567ed
VARUN.T.P <varunvarutp098@gmail.com>
2026-02-09 08:25:54 +0530

    Add files via upload

---
+++ 3A.py
@@ -0,0 +1,13 @@
+print("love") #output
+
+boy_name = input('Boy name:  ')
+boy_age = int(input("Boy age: "))
+girl_name = input('Girl Name:  ')
+girl_age = int(input("Girl age: "))
+
+#dvxcbbhujhyjkjcxv
+age_diff = abs(boy_age - girl_age)
+
+print(boy_name + "  loves  " + girl_name + ".age difference"  + str(age_diff))
+
+print(f"{boy_name}  loves  {girl_name}. age difference is {age_diff}")
